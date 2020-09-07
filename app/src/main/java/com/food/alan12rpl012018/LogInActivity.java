6ed562f084a2d63c3fdfda7b45f129429ed562e1
alanichsan <alanxrpl2018smkrus@gmail.com>
2020-09-07 14:57:22 +0700

    login and register

--- app/src/main/java/com/food/alan12rpl012018/LogInActivity.java
+++ app/src/main/java/com/food/alan12rpl012018/LogInActivity.java
@@ -2,8 +2,10 @@ package com.food.alan12rpl012018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.util.LocaleData;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,12 +19,14 @@ import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
-import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LogInActivity extends AppCompatActivity {
     TextView textView,forget; Button button; EditText txtemail,txtpassword;
     public static final String SHARED_PREFS = "sharedPrefs";
+    private  SharedPreferences preferences;
     public static final String ID = "id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,53 +52,52 @@ public class LogInActivity extends AppCompatActivity {
                 String email = txtemail.getText().toString().trim();
                 String password = txtpassword.getText().toString().trim();
 
-                boolean isEmpty = false;
+                HashMap<String, String> body = new HashMap<>();
+                body.put("email", email);
+                body.put("password", password);
 
-                if (email.isEmpty()) {
-                    isEmpty = true;
-                    txtemail.setError("Email harus diisi");
-                }
-
-                if (password.isEmpty()) {
-                    isEmpty = true;
-                    txtpassword.setError("Password harus diisi");
-                }
-
-                if (!isEmpty) {
-                AndroidNetworking.post("192.168.43.65/tugas_api/login.php")
-                        .addBodyParameter("email", email)
-                        .addBodyParameter("password", password)
-                        .setTag("test")
+                AndroidNetworking.post("http://192.168.6.89/tugas_api/login.php")
+                        .addBodyParameter(body)
                         .setPriority(Priority.MEDIUM)
                         .build()
                         .getAsJSONObject(new JSONObjectRequestListener() {
                             @Override
                             public void onResponse(JSONObject response) {
-                                try {
-                                    String status = response.getString("status");
-                                    String message = response.getString("message");
-                                    if (status.equals("success")) {
-                                        JSONObject data = response.getJSONObject("data");
-                                        String name = data.getString("name");
-                                        String id = data.getString("id");
+                                Log.d("ALN" , response.toString());
+                                String status = response.optString("status");
+                                String message = response.optString("mesagge");
+                                if (status.equalsIgnoreCase("SUCCESS")) {
+                                    JSONObject payload = response.optJSONObject("PAYLOAD");
+                                    String LOGIN_ID = payload.optString("LOGIN_ID");
+                                    String LOGIN_NAME = payload.optString("LOGIN_NAME");
+                                    String NOMOR_HP = payload.optString("NOMOR_HP");
+                                    String NOMOR_KTP = payload.optString("NOMOR_KTP");
+                                    String ALAMAT = payload.optString("ALAMAT");
+                                    String ROLE = payload.optString("ROLE");
 
-                                        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-                                        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                                        editor.putString(ID, id);
-                                        editor.apply();
-
+                                    preferences = getSharedPreferences("db_rentalsepeda", Context.MODE_PRIVATE);
+                                    preferences.edit()
+                                            .putString(LOGIN_ID, LOGIN_ID)
+                                            .putString(LOGIN_NAME, LOGIN_NAME)
+                                            .putString(NOMOR_HP, NOMOR_HP)
+                                            .putString(NOMOR_KTP, NOMOR_KTP)
+                                            .putString(ALAMAT, ALAMAT)
+                                            .putString(ROLE, ROLE)
+                                            .apply();
+                                    if (ROLE.equalsIgnoreCase("Customer")) {
                                         Intent intent = new Intent(LogInActivity.this, DashboardActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
-
-                                        Toast.makeText(LogInActivity.this, "Selamat datang " + name, Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    } else {
                                         Toast.makeText(LogInActivity.this, message, Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }else if (ROLE.equalsIgnoreCase("admin")){
+                                        Intent intent = new Intent(LogInActivity.this, DashboardActivity.class);
+                                        startActivity(intent);
+                                        Toast.makeText(LogInActivity.this, message, Toast.LENGTH_SHORT).show();
+                                        finish();
                                     }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
+                                }
+                                else {
+                                    Toast.makeText(LogInActivity.this, message, Toast.LENGTH_SHORT).show();
                                 }
                             }
                             @Override
@@ -103,7 +106,6 @@ public class LogInActivity extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(),"Gagal",Toast.LENGTH_LONG).show();
                             }
                         });
-            }
             }
         });
         forget.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/food/alan12rpl012018/RegisterActivity.java
+++ app/src/main/java/com/food/alan12rpl012018/RegisterActivity.java
@@ -3,19 +3,42 @@ package com.food.alan12rpl012018;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     TextView textView;
     Button button;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String ID = "id";
+    public static final String USERNAME = "username";
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         textView = findViewById(R.id.login);
+        final EditText txtemail = findViewById(R.id.email);
+        final EditText txtusername1 = findViewById(R.id.username);
+        final EditText txtnohp = findViewById(R.id.nohp);
+        final EditText txtnoktp = findViewById(R.id.noktp);
+        final EditText txtalamat = findViewById(R.id.address);
+        final EditText txtpass1 = findViewById(R.id.password1);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,8 +50,45 @@ public class RegisterActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this,DashboardActivity.class);
-                startActivity(intent);
+                String email = txtemail.getText().toString();
+                String username1 = txtusername1.getText().toString();
+                String nohp = txtnohp.getText().toString();
+                String noktp = txtnoktp.getText().toString();
+                String alamat = txtalamat.getText().toString();
+                String pass = txtpass1.getText().toString();
+
+                HashMap<String, String> body = new HashMap<>();
+                body.put("noktp", noktp);
+                body.put("email", email);
+                body.put("password", pass);
+                body.put("nama", username1);
+                body.put("nohp", nohp);
+                body.put("alamat", alamat);
+                body.put("role", "1");
+                AndroidNetworking.post("http://192.168.6.89/tugas_api/register.php")
+                        .addBodyParameter(body)
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("ALN" , response.toString());
+                                String status = response.optString("status");
+                                String message = response.optString("mesagge");
+                                if (status.equalsIgnoreCase("SUCCESS")) {
+                                    Intent intent = new Intent(RegisterActivity.this, DashboardActivity.class);
+                                    startActivity(intent);
+                                }
+                                else {
+                                    Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                Toast.makeText(RegisterActivity.this, "Kesalahan Internal", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
     }
